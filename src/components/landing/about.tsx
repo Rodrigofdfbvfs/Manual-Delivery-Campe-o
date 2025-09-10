46f37d6e2be9492776b82f3be4443c3af2ec8577
Paloma Emanuelly <emanuwaveh@gmail.com>
2025-09-10 13:30:42 +0000

    delete (_for element div_)

--- src/components/landing/about.tsx
+++ src/components/landing/about.tsx
@@ -4,27 +4,16 @@ export default function About() {
   return (
     <section id="about" className="py-12 sm:py-24">
       <div className="container mx-auto px-4">
-        <div className="grid grid-cols-1 gap-12 md:grid-cols-5 items-center">
-          <div className="md:col-span-2">
-            <Image
-              src="/vikto-profile.png"
-              alt="Foto do seu mentor, Vikto"
-              width={400}
-              height={400}
-              className="rounded-full aspect-square object-cover mx-auto w-64 h-64 md:w-full md:h-auto border-4 border-primary shadow-lg"
-            />
-          </div>
-          <div className="md:col-span-3">
+        <div className="text-center">
             <h2 className="text-3xl font-bold tracking-tight text-primary sm:text-4xl font-headline">
               SOBRE O SEU MENTOR, VIKTO
             </h2>
-            <p className="mt-6 text-lg font-light leading-8 text-foreground/80 font-headline">
+            <p className="mt-6 text-lg font-light leading-8 text-foreground/80 font-headline max-w-3xl mx-auto">
               Com mais de 4 anos de experiência no campo de batalha do tráfego pago, hoje lidero uma agência que já foi o motor de crescimento para mais de 100 clientes no ramo de delivery.
             </p>
-            <p className="mt-4 text-lg font-light leading-8 text-foreground/80 font-headline">
+            <p className="mt-4 text-lg font-light leading-8 text-foreground/80 font-headline max-w-3xl mx-auto">
               Já gerenciei mais de R$500.000 em anúncios, transformando investimentos em lucro e negócios locais em marcas de sucesso. Agora, condensei todo esse conhecimento prático no Manual do Delivery Campeão para que você também possa trilhar esse caminho.
             </p>
-          </div>
         </div>
       </div>
     </section>
