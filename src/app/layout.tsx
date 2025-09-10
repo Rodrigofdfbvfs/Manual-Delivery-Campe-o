62edad1e969543f5dfc3a700a302672c7ee68b43
Paloma Emanuelly <emanuwaveh@gmail.com>
2025-09-10 13:54:10 +0000

    altere todos os textos para open sans

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&family=Oswald:wght@300;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
         {children}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
     extend: {
       fontFamily: {
         body: ["'Open Sans'", 'sans-serif'],
-        headline: ["'Oswald'", 'sans-serif'],
+        headline: ["'Open Sans'", 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
