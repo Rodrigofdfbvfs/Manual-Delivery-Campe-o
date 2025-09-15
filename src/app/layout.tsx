c0b59840e332b1fdecc17934e8a46b8732d30099
Paloma Emanuelly <emanuwaveh@gmail.com>
2025-09-15 12:58:44 +0000

    Instale o pixel
    <!-- Meta Pixel Code -->
    <script>
    !function(f,b,e,v,n,

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import Preloader from '@/components/preloader';
 import SmoothScroll from '@/components/smooth-scroll';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Manual do Delivery Campeão',
@@ -21,8 +22,27 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700;800&display=swap" rel="stylesheet" />
         <link rel="icon" href="https://cdn-icons-png.flaticon.com/512/4387/4387430.png" type="image/png" />
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '4234400090174570');
+            fbq('track', 'PageView');
+          `}
+        </Script>
       </head>
       <body className="font-body antialiased">
+        <noscript>
+          <img height="1" width="1" style={{display: 'none'}}
+            src="https://www.facebook.com/tr?id=4234400090174570&ev=PageView&noscript=1"
+          />
+        </noscript>
         <Preloader />
         {children}
         <Toaster />
